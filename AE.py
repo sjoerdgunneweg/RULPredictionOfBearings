23e333051eed3f931ef17c0b596a59aee16fab90
sjoerdgunneweg <sjoerd.gunneweg@gmail.com>
2024-04-22 14:32:34 +0200

    structure for SAE and SVDAE

---
+++ AE.py
@@ -0,0 +1,20 @@
+class AutoEncoder:
+
+    def __init__(self,
+                 input_shape,
+                 conv_filters,
+                 conv_kernels,
+                 conv_strides,
+                 latent_space_dim):
+        
+        self.input_shape = input_shape
+        self.conv_filters = conv_filters
+        self.conv_kernels = conv_kernels
+        self.conv_strides = conv_strides
+        self.latent_space_dim = latent_space_dim
+
+        self.encoder = None
+        self.decoder = None
+        self.model = None
+
+        self._num_conv_layers = len(conv_filters)
